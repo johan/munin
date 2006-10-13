196ebf57a7b9be87d3b7ae548b3c91e4e4d2b712
Tore Anderson <tore@redpill-linpro.com>
2006-10-13 13:43:00 +0000

    Improve the workaround for Ubuntu, so that they respect the permissions defined in the statoverride.  Also add a equivalent workaround to munin's cron job, as it doesn't have a init script.
    Remove some dead code in munin-node's init script that was only used if
    running under (d)ash - it is always run under bash anyway, so it wasn't
    really needed.
    
    
    git-svn-id: svn://munin-monitoring.org/munin/branches/debian@1122 47311dc8-50f3-0310-b975-824c5c5ab7df

--- etch/debian/munin.cron.d
+++ etch/debian/munin.cron.d
@@ -4,6 +4,6 @@
 
 MAILTO=root
 
-@reboot         root  if [ ! -d /var/run/munin ]; then mkdir /var/run/munin; chmod 0755 /var/run/munin; chown munin:root /var/run/munin; fi
+@reboot         root  if [ ! -d /var/lib/munin ]; then /bin/bash -c 'perms=(`/usr/sbin/dpkg-statoverride --list /var/run/munin`); mkdir /var/lib/munin; if [ ! -z "$perms" ]; then chown ${perms[0]}:${perms[1]} /var/lib/munin; chmod ${perms[2]} /var/lib/munin; else chown munin:munin /var/lib/munin; chmod 0755 /var/lib/munin; fi'; fi
 */5 * * * *     munin if [ -x /usr/bin/munin-cron ]; then /usr/bin/munin-cron; fi
 14 10 * * *     munin if [ -x /usr/share/munin/munin-limits ]; then /usr/share/munin/munin-limits --force --contact nagios --contact old-nagios; fi
