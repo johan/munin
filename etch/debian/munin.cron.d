35e440aec9b98fd6c7cde36c0c625fd948a7013d
Tore Anderson <tore@redpill-linpro.com>
2006-10-13 13:00:41 +0000

    Make sure the /var/run/munin directory is created even though munin-node isn't installed, and make sure the permissions are the same as the statoverride (fix munin-node's init script)
    git-svn-id: svn://munin-monitoring.org/munin/branches/debian@1119 47311dc8-50f3-0310-b975-824c5c5ab7df

--- etch/debian/munin.cron.d
+++ etch/debian/munin.cron.d
@@ -4,5 +4,6 @@
 
 MAILTO=root
 
+@reboot         root  if [ ! -d /var/run/munin ]; then mkdir /var/run/munin; chmod 0755 /var/run/munin; chown munin:root /var/run/munin; fi
 */5 * * * *     munin if [ -x /usr/bin/munin-cron ]; then /usr/bin/munin-cron; fi
 14 10 * * *     munin if [ -x /usr/share/munin/munin-limits ]; then /usr/share/munin/munin-limits --force --contact nagios --contact old-nagios; fi
