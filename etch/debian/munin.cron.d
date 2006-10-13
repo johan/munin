19641de6dcd05683fbbc8b958f5c1eeddc5890d0
Tore Anderson <tore@redpill-linpro.com>
2006-10-13 22:09:54 +0000

    Make the Ubuntu /var/run/munin-gone-AWOL-workaround a bit prettier
    git-svn-id: svn://munin-monitoring.org/munin/branches/debian@1126 47311dc8-50f3-0310-b975-824c5c5ab7df

--- etch/debian/munin.cron.d
+++ etch/debian/munin.cron.d
@@ -4,6 +4,6 @@
 
 MAILTO=root
 
-@reboot         root  if [ ! -d /var/run/munin ]; then /bin/bash -c 'perms=(`/usr/sbin/dpkg-statoverride --list /var/run/munin`); mkdir /var/run/munin; if [ ! -z "$perms" ]; then chown ${perms[0]}:${perms[1]} /var/run/munin; chmod ${perms[2]} /var/run/munin; else chown munin:root /var/run/munin; chmod 0755 /var/run/munin; fi'; fi
+@reboot         root  if [ ! -d /var/run/munin ]; then /bin/bash -c 'perms=(`/usr/sbin/dpkg-statoverride --list /var/run/munin`); mkdir /var/run/munin; chown ${perms[0]:-munin}:${perms[1]:-root} /var/run/munin; chmod ${perms[2]:-0755} /var/run/munin'; fi
 */5 * * * *     munin if [ -x /usr/bin/munin-cron ]; then /usr/bin/munin-cron; fi
 14 10 * * *     munin if [ -x /usr/share/munin/munin-limits ]; then /usr/share/munin/munin-limits --force --contact nagios --contact old-nagios; fi
