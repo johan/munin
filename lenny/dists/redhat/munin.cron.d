f7ba209e9fc1665f0a7ec5f0d4c277b8ff20b41b
Matthias Schmitz <matthias@sigxcpu.org>
2008-04-17 19:16:31 +0000

    Remove the source from this branch -> from now only the debian/ directory should live here
    git-svn-id: svn://munin-monitoring.org/munin/branches/debian@1592 47311dc8-50f3-0310-b975-824c5c5ab7df

--- lenny/dists/redhat/munin.cron.d
+++
@@ -1,8 +0,0 @@
-#
-# cron-jobs for munin
-#
-
-MAILTO=root
-
-*/5 * * * *     munin test -x /usr/bin/munin-cron && /usr/bin/munin-cron
-14 10 * * *     munin test -x /usr/share/munin/munin-limits && /usr/share/munin/munin-limits --force --contact nagios --contact old-nagios
